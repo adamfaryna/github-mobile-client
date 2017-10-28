08bfab94ca96941fffdadfc18eaf7992ae6ca825
Adam Faryna <adamfaryna@gmail.com>
2017-10-28 22:21:07 +0100

    Added index.js for screens folder.

---
+++ src/screens/index.js
@@ -0,0 +1,21 @@
+/*
+ * Copyright (C) 2017 Adam Faryna <adamfaryna@appdy.net>
+ *
+ * Distributed under terms of the Proprietary license.
+ */
+
+export LoginScreen from './LoginScreen';
+export MainScreen from './MainScreen';
+export OrganizationsListScreen from './OrganizationsListScreen';
+export ProfileScreen from './ProfileScreen';
+export RepositoriesListScreen from './RepositoriesListScreen';
+export RepositoryDetailsScreen from './RepositoryDetailsScreen';
+
+/* export default { */
+/* 	LoginScreen, */
+/* 	MainScreen, */
+/* 	OrganizationsListScreen, */
+/* 	ProfileScreen, */
+/* 	RepositoriesListScreen, */
+/* 	RepositoryDetailsScreen */
+/* }; */
