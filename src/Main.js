c469f2faedce6adfaf0d2d5b70c20ad620ce0ac6
Adam Faryna <adamfaryna@gmail.com>
2017-10-28 22:22:12 +0100

    Added navigation configuration.

--- src/Main.js
+++ src/Main.js
@@ -4,5 +4,61 @@
  * Distributed under terms of the Proprietary license.
  */
 
-export default from './screens/LoginScreen';
+import React from 'react';
+import { StackNavigator, TabNavigator } from 'react-navigation';
 
+import {
+  LoginScreen,
+  MainScreen,
+  OrganizationsListScreen,
+  ProfileScreen,
+  RepositoriesListScreen,
+  RepositoryDetailsScreen
+} from './screens';
+
+const RepositoriesNavigator = StackNavigator({
+	Repositories: {
+		screen: RepositoriesListScreen
+	},
+	Repository: {
+		screen: RepositoryDetailsScreen
+	}
+});
+
+const MainNavigator = TabNavigator({
+	Organizations: {
+		screen: OrganizationsListScreen,
+		navigationOptions: {
+			headerTitle: 'Organizations'
+		}
+	},
+	Repositories: {
+		screen: RepositoriesNavigator,
+		navigationOptions: {
+			headerTitle: 'Repositories'
+		}
+	},
+	Profile: {
+		screen: ProfileScreen,
+		navigationOptions: {
+			headerTitle: 'Profile'
+		}
+	}
+});
+
+const rootNavigator = StackNavigator({
+  Login: {
+    screen: LoginScreen,
+    navigationOptions: {
+      headerTitle: 'Login'
+    }
+  },
+  Main: {
+    screen: MainNavigator,
+    navigationOptions: {
+      headerTitle: 'Main'
+    }
+  }
+});
+
+export default rootNavigator;
